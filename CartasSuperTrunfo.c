5631d24665a813bb4f149ef8a696cef59487f17a
Luciscb <lucianasouzacastelo04@gmail.com>
2025-02-23 17:52:26 +0000

    cartas 1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 
     int main(){
-        int População, Pontos;
-        float Área, PIB;
-        char Estado, Cidade, Código[50];
+        int Populacao1 = 3942000, Populacao2 = 14140000, Pontos1 = 30, Pontos2 = 40;
+        float Area1 = 1.571, Area2 = 567.265, PIB1 = 49.19, PIB2 = 114.4;
+        char Estado1[50] = "Amazonas", Estado2[50] = "Bahia", Codigo1[50] = "A01", Codigo2[50] = "B01", Cidade1[50] = "Manaus", Cidade2[50] = "Salvador";
+        int Carta1 = 1, Carta2 = 2;
+       
+        printf("Carta %d\n", Carta1);
+        printf("Estado: %s\n", Estado1);
+        
+        printf("Código: %s\n", Codigo1);
 
-        printf("Carta 01\n");
-        scanf("%s", &Estado);
-        printf("Estado: %s\n", Estado);
+        printf("Cidade: %s\n", Cidade1);
 
-        scanf("%s", &Código);
-        printf("código: %s\n", Código);
+        printf("População: %d\n", Populacao1);
 
-        scanf("%s", &Cidade);
-        printf("Cidade: %s\n", Cidade);
+        printf("Area: %fkm²\n", Area1);
 
-        scanf("%d", &População);
-        printf("População: %d\n", População);
+        printf("PIB: %f bilhões de reais\n", PIB1);
 
-        scanf("%f", &Área);
-        printf("Área: %f\n", Área);
+        printf("Pontos: %d\n", Pontos1);
+        
+        printf("\n");
+        
+        printf("Carta %d\n", Carta2);
+        printf("Estado: %s\n", Estado2);
+    
+        printf("Codigo: %s\n", Codigo2);
+    
+        printf("Cidade: %s\n", Cidade2);
+        
+        printf("Populacao: %d\n", Populacao2);
 
-        scanf("%f", &PIB);
-        printf("PIB: %f\n", PIB);
+        printf("Area: %fkm²\n", Area2);
 
-        scanf("%d", &Pontos);
-        printf("Pontos: %d\n", Pontos);
+        printf("PIB: %f bilhões de reais\n", PIB2);
+
+        printf("Pontos: %d\n", Pontos2);
 
         return 0;
-
+    
 }
+    
+
+
+        
         
 
 
